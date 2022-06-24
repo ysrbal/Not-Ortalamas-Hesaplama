2b29aee5fa029ba410934e5b00f2e83002e71cc0
ysrbal <ysr.bal@gmail.com>
2022-06-24 08:43:44 +0300

    input veri ismi puan olarak değiştirildi.

--- Not_Ortalamasi_Hesaplama.java
+++ Not_Ortalamasi_Hesaplama.java
@@ -2,26 +2,26 @@ import java.util.Scanner;
 
 public class Not_Ortalamasi_Hesaplama {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
+        Scanner puan = new Scanner(System.in);
         int mat, fzk, kimya, turkce, tarih, muzik;
 
         System.out.print("Matematik Notunu Girin:");
-        mat=input.nextInt();
+        mat=puan.nextInt();
 
         System.out.print("Fizik Notunu Girin:");
-        fzk=input.nextInt();
+        fzk=puan.nextInt();
 
         System.out.print("Kimya Notunu Girin:");
-        kimya=input.nextInt();
+        kimya=puan.nextInt();
 
         System.out.print("Turkce Notunu Girin:");
-        turkce=input.nextInt();
+        turkce=puan.nextInt();
 
         System.out.print("Tarih Notunu Girin:");
-        tarih=input.nextInt();
+        tarih=puan.nextInt();
 
         System.out.print("Muzik Notunu Girin:");
-        muzik=input.nextInt();
+        muzik=puan.nextInt();
 
         int toplam = (mat+fzk+kimya+turkce+tarih+muzik);
         double sonuc = toplam/6.0;
